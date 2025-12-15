3f436fabbdae54a762b3b267d9236a754bc99021
jari.tenhunen <jari.tenhunen@outlook.com>
2025-12-15 12:34:44 +0100

    navbar, router and bootstrap added, trying to keep default css file and work from that

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import Home from "./Home.jsx";
+import NavBar from "./components/NavBar.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+);
